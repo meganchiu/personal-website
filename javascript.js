0f09a003b6c3eeb36d971e84d9586f29ffbb6b74
Megan Chiu <megan.chiu.2014@gmail.com>
2024-12-06 21:58:42 -0800

    contact page logic and some styling changes

--- javascript.js
+++ javascript.js
@@ -1,2 +1,9 @@
-const greeting = "Hello world!";
-alert(greeting);
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+console.log(window.location.origin)
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/Users/megan.chiu/Documents/Coursework/block09/personal-website/success.html'
+})
---
+++ success.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+  window.location.href = window.location.origin + '/Users/megan.chiu/Documents/Coursework/block09/personal-website/home.html'
+}, 5000)
